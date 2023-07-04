712c6570101d5dcd6791e83e1dcdeaf83c615368
Abderrahmane <tiamani3939@gmail.com>
2023-07-04 13:00:39 +0100

    project card

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,77 +5,55 @@ import { projects } from "../contants";
 
 const ProjectCard = ({ project }) => {
   return (
-    <div className="card" data-aos="fade-right" data-aos-once="true">
-      <div className="content">
-        <div className="back">
-          <div className="back-content">
-            <img
-              src={project.image}
-              className="project__img"
-              alt={project.name}
-              loading="lazy"
-            />
-            <strong>
-              <span className="blue">{project.name}</span>
-            </strong>
-            {project.tech}
-          </div>
+    <>
+      <article class="card" data-aos="fade-right" data-aos-once="true">
+        <div>
+          <img
+            src={project.image}
+            className="project__img"
+            alt={project.name}
+            loading="lazy"
+          />
         </div>
-        <div className="front">
-          <div className="img">
-            <div className="circle"></div>
-            <div className="circle" id="right"></div>
-            <div className="circle" id="bottom"></div>
-          </div>
-
-          <div className="front-content">
-            <small className="badge">
-              <div className="project__description--para">
-                {project.para}
-              </div>
-            </small>
-            <div className="description">
-              <div className="card-footer">
-                <div>
+        <div class="card_content">
+          <span class="card_title">
+              {project.name}
+          </span>
+          <span class="card_subtitle">{project.tech}</span>
+          <p class="card_description">{project.para}</p>
+          <div class="card_description">
                   <a
                     href={project.github}
                     target={"_blank"}
                     className="project__description--link click"
                   >
-                    <img src={github} alt="" className="ic__img-" loading="lazy"/>
+                    <img
+                      src={github}
+                      alt=""
+                      className="ic__img-"
+                      loading="lazy"
+                    />
                   </a>
                   <a
                     href={project.link}
                     className="project__description--link click"
                     target={"_blank"}
                   >
-                    <FontAwesomeIcon
-                      icon="fa-link"
-                      className="ic__img--"
-                    />
+                    <FontAwesomeIcon icon="fa-link" className="ic__img--" />
                   </a>
                 </div>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
+      </article>
+    </>
   );
 };
-
-
 
 function Projects() {
   return (
     <section id="projects">
       <div className="row">
         <div className="projects__wrapper">
-          <h1
-            className="proj__header"
-            data-aos="fade-up"
-            data-aos-once="true"
-          >
+          <h1 className="proj__header" data-aos="fade-up" data-aos-once="true">
             Here are some of my <span className="blue">projects:</span>{" "}
           </h1>
           <div className="project__list">
--- src/contants/index.js
+++ src/contants/index.js
@@ -1,7 +1,6 @@
 import css from "../assets/tech/css1.png";
 import html from "../assets/tech/html1.png";
 import javascript from "../assets/tech/js.png";
-import ts from "../assets/tech/ts1.png";
 import next from "../assets/tech/next1.png";
 import reactjs from "../assets/tech/react.png";
 import tailwind from "../assets/tech/tailwind.png";
@@ -19,20 +18,12 @@ const technologies = [
   { name: "NextJS", icon: next },
   { name: "TailwindCss", icon: tailwind },
 ];
-import ultra from "../assets/ultra.png";
+
 import movieheaven from "../assets/movieHeaven.png";
 import fakeshop from "../assets/fakeShop.png";
-import chatApp from "../assets/chatapp.png";
-import carcatalogue from "../assets/carcatalogue.png";
+import rmp from '../assets/rmp.png'
+
 const projects = [
-  {
-    name: " Car Catalogue   ",
-    image: carcatalogue,
-    tech: "Nextjs 13, Typescript, TailwindCSS",
-    para: "•Developed and implemented a modern web application utilizing Next.js 13, TypeScript, and Tailwind CSS, providing advanced search functionality with filtering capabilities, resulting in a user-friendly experience and increased customer engagement.",
-    github: "https://github.com/Abde-T/carcatalogue",
-    link: "https://carcatalogue.vercel.app/",
-  },
   {
     name: "Fake shop",
     image: fakeshop,
@@ -50,20 +41,12 @@ const projects = [
     link: "https://movieheaven.vercel.app/",
   },
   {
-    name: " Twitter Clone  ",
-    image: ultra,
-    tech: "Next.js, Redux, Tailwindcss, Firebase",
-    para: "Created a social media application inspired by Twitter, built with Next.js, Redux, and Firebase. Empowering users with the ability to post tweets, engage in lively discussions through comments, and seamlessly log in to the platform.",
-    github: "https://github.com/Abde-T/socialmedia",
-    link: "https://social-media-abde-t.vercel.app/",
-  },
-  {
-    name: " ChatApp  ",
-    image: chatApp,
-    tech: "Node.js, Expressjs, Socket.io",
-    para: "Engineered a cutting-edge chat application in MERN stack, utilizing Node.js and Socket.io to deliver instantaneous messaging capabilities for users across multiple chat rooms, resulting in improved user engagement and satisfaction.",
-    github: "https://github.com/Abde-T/chat_app",
-    link: "https://chatapp-ybq3.onrender.com/",
+    name: "Rate My Portfolio",
+    image: rmp,
+    tech: "MERN stack",
+    para: 'Developed a high-performance website using the MERN stack, allowing developers to create an account and upload their portfolios for review, providing a platform for showcasing their skills and receiving feedback from peers. It leverages MongoDB, Express.js, React, and Node.js to deliver a seamless user experience and robust functionality.',
+    github: "https://github.com/Abde-T/rmp",
+    link: "https://rmp-abde-t.vercel.app/",
   },
 ];
 export { projects };
