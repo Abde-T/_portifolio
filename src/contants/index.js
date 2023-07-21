125cb919b3a845bcd18b27d9d15d8de86bdf36c3
Abderrahmane <tiamani3939@gmail.com>
2023-07-21 17:12:24 +0100

    changes

--- src/contants/index.js
+++ src/contants/index.js
@@ -22,8 +22,7 @@ const technologies = [
 
 import movieheaven from "../assets/movieHeaven.png";
 import fakeshop from "../assets/fakeShop.png";
-// import rmp from '../assets/rmp.png'
-import ultra from "../assets/ultra.png";
+import rmp from '../assets/rmp.png'
 
 const projects = [
   {
@@ -42,23 +41,15 @@ const projects = [
     github: "https://github.com/Abde-T/movieheaven",
     link: "https://movieheaven.vercel.app/",
   },
-  // {
-  //   name: "Rate My Portfolio",
-  //   image: rmp,
-  //   tech: "MERN stack",
-  //   para: 'Developed a high-performance website using the MERN stack, allowing developers to create an account and upload their portfolios for review, providing a platform for showcasing their skills and receiving feedback from peers. It leverages MongoDB, Express.js, React, and Node.js to deliver a seamless user experience and robust functionality.',
-  //   github: "https://github.com/Abde-T/rmp",
-  //   link: "https://temp-rj2kt6vk7-abde-t.vercel.app/",
-  // },
-
   {
-    name: "Ultraverse ",
-    image: ultra,
-    tech: "React, JavaScript ",
-    para: "Revamped website design by incorporating dynamic animations, transitions, and carousels to enhance user experience. Efficiently processed API requests using Axios to dynamically present data from the cloud server, leading to an overall reduction of page loading time.",
-    github: "https://github.com/Abde-T/Abde-internship/tree/Abde-animations",
-    link:"https://ultraverse-gamma.vercel.app/",
+    name: "Review My Project",
+    image: rmp,
+    tech: "MERN stack",
+    para: 'Developed a high-performance website using the MERN stack, allowing developers to create an account and upload their portfolios for review, providing a platform for showcasing their skills and receiving feedback from peers. It leverages MongoDB, Express.js, React, and Node.js to deliver a seamless user experience and robust functionality.',
+    github: "https://github.com/Abde-T/rmp",
+    link: "https://rmp-abde-t.vercel.app/",
   },
+
 ];
 export { projects };
 export { technologies };
